e260e8221b6787de80ed3f6624057146bacc9807
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-01-06 19:15:04 +0530

    BasicCoreProgram

---
+++ source/repos/BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicCoreProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
